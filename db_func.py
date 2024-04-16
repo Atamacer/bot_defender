002cd11e2ce0085c18d8a74e19f1c694ccfc95ab
Даниил <dan4ik-gp@yandex.ru>
2024-04-16 16:25:01 +0300

    готовы функции для БД

---
+++ db_func.py
@@ -0,0 +1,36 @@
+import mariadb
+
+with open('db_config.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+connect = mariadb.connect(
+    user=lines[0],
+    password=lines[1],
+    host=lines[2],
+    port=int(lines[3]),
+    database=lines[4]
+)
+
+cur = connect.cursor()
+
+
+# добавление группы в БД
+def add_new_group(group_id: int, group_name: str):
+    cur.execute('INSERT INTO groups (group_id, group_name) VALUES (?, ?)',
+    (group_id, group_name))
+    connect.commit()
+
+
+# удаление группы из БД
+def delete_group(group_id: int):
+    cur.execute(f'DELETE FROM groups WHERE group_id = {group_id}')
+    connect.commit()
+
+
+# добавление подозрительного пользователся в БД
+def add_marked_user(user_id: int, text: str):
+    cur.execute('INSERT INTO marked_users (user_id, text) VALUES (?, ?)',
+                (user_id, text))
+    connect.commit()
+
+add_marked_user(123, 'лох')
